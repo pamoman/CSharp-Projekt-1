bc8ad660c34d944892aee40c0e681cb6fb7ef9fd
Paul Moreland <Paul.Moreland@edu.lidkoping.se>
2021-10-04 14:06:24 +0200

    Add project files.

---
+++ CSharp-Projekt-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Projekt_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
